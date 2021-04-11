f9c4cfdca4f160b4752042dffa425a724b5a85d3
gabriel <gabriel.devloper@gmail.com>
2021-04-11 19:35:10 -0300

    feat: extensions

--- ormconfig.ts
+++ ormconfig.ts
@@ -7,8 +7,11 @@ export default {
   username: "xgeqhdhm",
   password: "y3h6OKsRt3T7KgBsLp5SC9stC3MlE7n5",
   database: "xgeqhdhm",
-  migrations: ["./src/database/migrations/**.ts"],
-  entities: ["./src/models/**.ts"],
+  migrations: [
+    `./${process.env.DIRECTORY_TYPEORM}/database/migrations/**.${process.env.EXT}`,
+  ],
+  entities: [`./${process.env.DIRECTORY_TYPEORM}/models/**.${process.env.EXT}`],
+
   logging: true,
   cli: {
     entitiesDir: "src/models",

936494f3d81eba737c9d9832d6869a4f82598c04
Gabriel Barreto <gabriel.devloper@gmail.com>
2021-04-12 16:37:07 -0300

    feat: ajustes no ormconfig.

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,11 +2,7 @@
 
 module.exports = {
   type: "postgres",
-  host: "queenie.db.elephantsql.com",
-  port: 5432,
-  username: "xgeqhdhm",
-  password: "y3h6OKsRt3T7KgBsLp5SC9stC3MlE7n5",
-  database: "xgeqhdhm",
+  url: process.env.DATABASE_URL,
   migrations: [
     `./${process.env.DIRECTORY_TYPEORM}/database/migrations/**.${process.env.EXT}`,
   ],

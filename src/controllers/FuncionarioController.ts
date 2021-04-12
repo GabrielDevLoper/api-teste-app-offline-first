ff64de21448038bce0d93d23f48217a9a24aa5e6
gabriel <gabriel.devloper@gmail.com>
2021-04-11 22:18:22 -0300

    feat: validação de campos

--- src/controllers/FuncionarioController.ts
+++ src/controllers/FuncionarioController.ts
@@ -3,7 +3,7 @@ import { getCustomRepository } from "typeorm";
 import { AppError } from "../errors/AppError";
 import { FuncionarioRepository } from "../repositories/FuncionarioRepository";
 import { SetorRepository } from "../repositories/SetorRepository";
-
+import * as yup from 'yup';
 class FuncionarioController {
 
     async index(req: Request, res: Response){
@@ -17,6 +17,19 @@ class FuncionarioController {
     async store(req: Request, res: Response){
         const { nome, cpf, data_nascimento, id_setor } = req.body;
 
+        let schema = yup.object().shape({
+            nome: yup.string().required("Nome é obrigatorio"),
+            cpf: yup.string().required("Cpf obrigatorio"),
+            data_nascimento: yup.string().required("data nascimento obrigatorio"),
+            id_setor: yup.string().required("setor obrigatorio"),
+        });
+
+        try {
+            await schema.validate(req.body);
+        } catch (error) {
+            throw new AppError(error, 200);
+        }
+
         const funcionarioRepository = getCustomRepository(FuncionarioRepository);
         const setorRepository = getCustomRepository(SetorRepository);
 
@@ -25,22 +38,27 @@ class FuncionarioController {
         const existsSetor = await setorRepository.findOne({id: id_setor});
 
         if(!existsSetor){
-            throw new AppError("Setor not already exists", 404)
+            throw new AppError("Setor não existe", 200)
         }
 
         if(existsFuncionario){
-            throw new AppError("Funcionario already exists", 400)
+            throw new AppError("Funcionario já está cadastrado", 200)
         }
 
         const date = data_nascimento.split("/");
         const dateFormat = `${date[2]}-${date[1]}-${date[0]}`
 
+        if(new Date(dateFormat).toDateString() == "Invalid Date"){
+            throw new AppError("Data inserida inválida", 200)
+        }
+       
         const funcionario = funcionarioRepository.create({
             nome,
             cpf,
             data_nascimento: new Date(dateFormat),
             id_setor: existsSetor
         });
+
 
         await funcionarioRepository.save(funcionario);
 
